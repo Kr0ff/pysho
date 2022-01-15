74bf8ea2d8d4a571a9b55017503576dd8ab4244d
Kr0ff <kr0ff@localhost>
2022-01-15 19:02:50 +0000

    Update

--- pysho.py
+++ pysho.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import shodan
@@ -6,9 +6,11 @@ import sys
 import argparse
 from termcolor import colored
 from time import sleep
-from pathlib import Path
+# from pathlib import Path
 
-art = '''
+def artistic_view():
+
+    art = '''
 @@@@@@@   @@@ @@@   @@@@@@   @@@  @@@   @@@@@@
 @@@@@@@@  @@@ @@@  @@@@@@@   @@@  @@@  @@@@@@@@
 @@!  @@@  @@! !@@  !@@       @@!  @@@  @@!  @@@
@@ -21,36 +23,46 @@ art = '''
  :           :     :: : :     :   : :   : :  :
 
                 v0.1   @Kr0ff
+    '''
+    disclaimer = colored("              [!] Disclaimer [!]", "yellow")
 
-              [!] Disclaimer [!]
-
+    advice = colored('''
 [X] The author is not responsible how this tool is used !
 [X] You agree to take full responsibility for your actions !
 [X] This tool is to be used for educational purposes only !
 [X] Pentesting a service without written permissions is ILLEGAL !
-'''
-print(colored(art, "blue"))
+''', "red")
+
+    print(colored(art, "blue"))
+    print(disclaimer)
+    print(advice)
 
 #initiate API key check
 try:
-    init_key = Path("./API_KEY")
-    if init_key.is_file() and os.stat(init_key).st_size < 2:
-        with open("API_KEY", "w") as key_file:
-            print(colored("[!] 'API_KEY' file is empty...", "yellow"))
-            sleep(1)
+    init_key = f"{os.getcwd()}/API_KEY"
+    if not os.path.exists(init_key):
+        # if os.path.isfile(init_key) and os.stat(init_key).st_size < 2:
+        print(colored("[!] 'API_KEY' doesn't exist", "yellow"))
+        
+        k = input("[*] Enter your Shodan.io API Key: ")
+        while len(k) == 0 or len(k) < 32:
             k = input("[*] Enter your Shodan.io API Key: ")
-            while len(k) == 0 or len(k) < 32:
-                k = input("[*] Enter your Shodan.io API Key: ")
+        
+        with open(f"{os.getcwd()}/API_KEY", "w") as key_file:
             key_file.write(str(k))
-            print(colored("[+] API key saved !", "green"))
             key_file.close()
+        
+        print(colored("[+] API key saved !", "green"))
+        sleep(0.5)
+    
     else:
         print(colored("\r\n[+] API key exists in file 'API_KEY', Continuing...", "green"))
-        sleep(1)
-        with open("./API_KEY") as key_file:
+        # sleep(1)
+        with open(f"{os.getcwd()}/API_KEY", "r") as key_file:
             k = key_file.readline().rstrip('\n')
+
 except KeyboardInterrupt:
-    print(colored("[!] KeyboardInterrupt: Exitting !", "yellow"))
+    print(colored("\r\n[!] KeyboardInterrupt: Exitting !", "yellow"))
     sys.exit(0)
 
 #Clear screen after API_KEY check
@@ -63,35 +75,39 @@ connect = shodan.Shodan(k)
 def key_info():
     try:
         key = connect.info()
-        print("[*] Scan Credits: %s" % colored(key['scan_credits'], "green"))
-        print("[*] Query Credits: %s" % colored(key['query_credits'], "green"))
-        print("[*] Monitored IPs: %s" % colored(key['monitored_ips'], "green"))
-        print("[*] Plan: %s" % colored(key['plan'], "green"))
-        print("[*] HTTPS ?: %s" % colored(key['https'], "white"))
-        print("[*] Unlocked ?: %s" % colored(key['unlocked'], "white"))
-        print("[*] Telnet ?: %s" % colored(key['telnet'], "white"))
-        print("[*] Unlocked Left: %s" % colored(key['unlocked_left'], "green"))
+        print(f"[*] Scan Credits: {colored(key['scan_credits'], 'green')}")
+        print(f"[*] Query Credits: {colored(key['query_credits'], 'green')}")
+        print(f"[*] Monitored IPs: {colored(key['monitored_ips'], 'green')}")
+        print(f"[*] Plan: {colored(key['plan'], 'green')}")
+        print(f"[*] HTTPS ?: {colored(key['https'], 'white')}")
+        print(f"[*] Unlocked ?: {colored(key['unlocked'], 'white')}")
+        print(f"[*] Telnet ?: {colored(key['telnet'], 'white')}")
+        print(f"[*] Unlocked Left: {colored(key['unlocked_left'], 'green')}")
     except shodan.APIError as e:
-        print(colored('[-] API Error: {}'.format(e), "red"))
+        print(colored(f'[-] API Error: {e}', "red"))
 
 #Lookup by IP
 def host(i):
     try:
         try:
             print("[+] Looking up {}\r\n".format(colored(str(i), "cyan")))
+            
             #Lookup the host
             findings = connect.host(str(i))
+            
             #Print general info
-            print("[#] Organization: {}".format(colored(findings['org'], "green")))
-            print("[#] Operating System: {}".format(colored(findings['os'], "green")))
+            print(f"[#] Organization: {colored(findings['org'], 'green')}")
+            print(f"[#] Operating System: {colored(findings['os'], 'green')}")
             for result in findings['data']:
-                print("[#] Port: {}".format(colored(result['port'], "green")))
+                print(f"[#] Port: {colored(result['port'], 'green')}")
                 print("[>>] Banner:")
-                print("\r\n{}".format(colored(result['data'], "magenta")))
+                print(f"\r\n{colored(result['data'], 'magenta')}")
                 print(colored("-+"*50, "white"))
+        
         except shodan.APIError as e:
-            print(colored('[-] API Error: {}'.format(e), 'red'))
+            print(colored(f'[-] API Error: {e}', 'red'))
             sleep(0.5)
+    
     except KeyboardInterrupt:
         print(colored("\r\n[!] KeyboardInterrupt: Exitting !", "yellow"))
         sys.exit(0)
@@ -100,27 +116,31 @@ def host(i):
 def ip_lookup(s):
     try:
         try:
-            print("[+] Searching for: {}\r\n".format(colored(s, "cyan")))
+            print(f"[+] Searching for: {colored(s, 'cyan')}\r\n")
+            
             # Search Shodan
             findings = connect.search(str(s))
+            
             # Show the results, if None raise exception
-            print('Results found: {}\r\n'.format(colored(findings['total'], "red", attrs=["bold"])))
-            sleep(1.5) #Leave a min to check total number of results found
+            print(f'Results found: {colored(findings["total"], "red", attrs=["bold"])}\r\n')
+            sleep(1.5) #Leave a moment to check total number of results found
+            
             for result in findings['matches']:
-                print('[#] IP: {}'.format(colored(result['ip_str'], "green")))
-                print('[#] Port: {}'.format(colored(result['port'], "green")))
-                print('[#] Organization: {}'.format(colored(result['org'], "green")))
-                print('[#] Location: {}'.format(colored(result['location'], "green")))
-                print('[#] Layer: {}'.format(colored(result['transport'], "green")))
-                print('[#] Domains: {}'.format(colored(result['domains'], "green")))
-                print('[#] Hostnames: {}'.format(colored(result['hostnames'], "green")))
+                print(f'[#] IP: {colored(result["ip_str"], "green")}')
+                print(f'[#] Port: {colored(result["port"], "green")}')
+                print(f'[#] Organization: {colored(result["org"], "green")}')
+                print(f'[#] Location: {colored(result["location"], "green")}')
+                print(f'[#] Layer: {colored(result["transport"], "green")}')
+                print(f'[#] Domains: {colored(result["domains"], "green")}')
+                print(f'[#] Hostnames: {colored(result["hostnames"], "green")}')
                 print("[>>] Banner: ")
-                print('{}'.format(colored(result['data'], "magenta")))
-                print(colored('-+'*50, "white"))
-                print("")
+                print(colored(result['data'], "magenta"))
+                print(colored('-+'*50, "white") + "\r\n")
+        
         except shodan.APIError as e:
-            print(colored('[-] API Error: {}'.format(e), "red"))
+            print(colored(f'[-] API Error: {e}', "red"))
             sleep(0.5)
+
     except KeyboardInterrupt:
         print(colored("\r\n[!] KeyboardInterrupt: Exitting !", "yellow"))
         sys.exit(0)
@@ -129,7 +149,8 @@ def ip_lookup(s):
 if __name__ == "__main__":
 
     #Print the beautiful art above :)
-    print(colored(art, "blue"))
+    # print(colored(art, "blue"))
+    artistic_view()
     #Setup the argument parser
     parser = argparse.ArgumentParser(description='Search Shodan.io using the Shodan API')
     parser.add_argument("-i", "--ip_address", help="Host to search by IP", type=str)
